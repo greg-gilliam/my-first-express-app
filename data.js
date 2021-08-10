3925e15500fe4e28d0bf5410b83ecc825d5548ab
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-08-10 15:40:19 -0700

    added more data

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+{
+    "id": 1,
+    name: 'Lemon Meringue',
+    icing: false,
+    type: pie
+},
+{
+   "id": 2,
+    name: 'Carrot Cake',
+    icing: true,
+    type: cake 
+},
+{
+    "id": 3,
+    name: 'Devils Food Cake',
+    icing: true,
+    type: cake
+}
+];
+
+module.exports = data; 

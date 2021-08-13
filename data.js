739132d032d569cd1afa892c0739894f18cc3cd0
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-08-12 21:57:31 -0700

    fixed data conflict

--- data.js
+++ data.js
@@ -1,16 +1,20 @@
 const data = [
-{
-    "id": 1,
-    name: 'Lemon Meringue'
-},
-{
-   "id": 2,
-    name: 'Carrot Cake'
-},
-{
-    "id": 3,
-    name: 'Devils Food Cake'
-}
+    {
+        "id": 1,
+        name: 'Lemon Meringue',
+        icing: false,
+        type: pie
+    },
+    {
+        "id": 2,
+        name: 'Carrot Cake',
+        icing: true,
+        type: cake
+    },
+    {
+        "id": 3,
+        name: 'Devils Food Cake',
+        icing: true,
+        type: cake
+    }
 ];
-
-module.exports = data; 
